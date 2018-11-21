10855d27d882aae7591440fd7317745314acc344
vishnupraneeth <vishnupraneeth96@gmail.com>
2018-11-21 06:53:09 -0800

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,22 @@
+package jAVASTRINGS;
+//import java.util.Scanner; 
+
+public class Test 
+	
+	
+	{ 
+	    public static void main(String[] args) 
+	    { 
+	        // Declare the object and initialize with 
+	        // predefined standard input object 
+	        //Scanner sc = new Scanner(System.in); 
+	   
+	        // Character input 
+	       // char c = sc.next().charAt(0); 
+	   
+	        // Print the read value 
+	      //  System.out.println("c = "+c); 
+	    } 
+	}
+
+
